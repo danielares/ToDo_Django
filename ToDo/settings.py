4a7a9954271a6e5c94fd27b3945bc4dd5b6491b4
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-04-30 12:09:06 -0300

    fixing error

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from django_key import SECRET_KEY_DJANGO
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rrqkwxbz^t&h&ha+6_sot$h)5otb#3=#65vkcy935ikpx2=_vg'
+SECRET_KEY = SECRET_KEY_DJANGO
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
