84973dd2aa7200e88ce6249bc162d988a535943b
Daniel Ares Moraes <daniel.ares@hotmail.com>
2022-04-30 12:22:28 -0300

    fixing deploy errors

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY_DJANGO
+SECRET_KEY = 'django-insecure-rrqkwxbz^t&h&ha+6_sot$h)5otb#3=#65vkcy935ikpx2=_vg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
